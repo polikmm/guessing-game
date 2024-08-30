d2c1ac2b6994281dacb1705e55df72134003d1dd
polikmm <polikmm@mail.ru>
2024-08-30 15:01:26 +0400

    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.array = [];
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.array.push(i);
+        }
+        return this.array;
     }
 
     guess() {
-
+        const middle = Math.floor((min + max) / 2);
+        return this.array[middle];
     }
 
     lower() {
-
+        this.array = this.array.slice(0, Math.floor((min + max) / 2));
+        return this.guess();
     }
 
     greater() {
-
+        this.array = this.array.slice(Math.floor((min + max) / 2));
+        return this.guess();
     }
 }
 
